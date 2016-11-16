166b51eb058b96b0f1b4c580c12100f23a00b1cd
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-16 16:58:10 +0900

    答え

--- CheckTest/ViewController.swift
+++ CheckTest/ViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var tableView: UITableView!
     
-    var contentArray: [String]
+    var contentArray: [String] = []
 
     override func viewDidLoad() {
         
@@ -25,16 +25,26 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     func createContents() {
         
-        for var i = 0; i < 10; i++ {
+        for i in 0 ..< 10 {
             
-            contentArray.append(i)
+            contentArray.append(String(i))
         }
         // ここでtableViewを更新
+        tableView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return contentArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell?.textLabel?.text = contentArray[indexPath.row]
+        
+        return cell!
     }
 }
 
