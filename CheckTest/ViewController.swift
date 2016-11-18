8e43c1c15f970aa2d439b7bbd6f1044a26121015
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-19 00:33:44 +0900

    [Change] tableView IBOutlet strong to weak

--- CheckTest/ViewController.swift
+++ CheckTest/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
     
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
     var contentArray: [String]
 
